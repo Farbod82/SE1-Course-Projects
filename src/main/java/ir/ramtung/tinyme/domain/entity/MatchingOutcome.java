06739548204af1d1dfde52756b181bfa2f426eeb
Farbod82 <farbodazimmohseni@gmail.com>
2024-04-10 11:29:18 +0330

    error handling and publishing done for getMinimumExecutionQuantity variable

--- src/main/java/ir/ramtung/tinyme/domain/entity/MatchingOutcome.java
+++ src/main/java/ir/ramtung/tinyme/domain/entity/MatchingOutcome.java
@@ -3,5 +3,7 @@ package ir.ramtung.tinyme.domain.entity;
 public enum MatchingOutcome {
     EXECUTED,
     NOT_ENOUGH_CREDIT,
-    NOT_ENOUGH_POSITIONS
+    NOT_ENOUGH_POSITIONS,
+
+    MINIMUM_TRADE_NOT_PASSED
 }
