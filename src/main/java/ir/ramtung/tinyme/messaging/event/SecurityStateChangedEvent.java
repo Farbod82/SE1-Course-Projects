ff52373d17fec31abfa6c609932311cad6fcbbe5
Ali <you@example.com>
2024-05-09 11:23:49 +0330

    add CahngeMach.stateRq

--- src/main/java/ir/ramtung/tinyme/messaging/event/SecurityStateChangedEvent.java
+++ src/main/java/ir/ramtung/tinyme/messaging/event/SecurityStateChangedEvent.java
@@ -1,6 +1,14 @@
 package ir.ramtung.tinyme.messaging.event;
 import ir.ramtung.tinyme.messaging.request.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
+@Data
+@EqualsAndHashCode(callSuper = false)
+@AllArgsConstructor
+@NoArgsConstructor
 public class SecurityStateChangedEvent extends Event{
     String securityIsin;
     MatchingState state;
