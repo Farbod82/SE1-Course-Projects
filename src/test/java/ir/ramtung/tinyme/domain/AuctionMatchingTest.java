8ec14a6fcf756a414069849ddb7b446812aa4852
amirmfarzane <102407739+amirmfarzane@users.noreply.github.com>
2024-05-10 21:05:13 +0430

    add test for iop checking in auction matching

---
+++ src/test/java/ir/ramtung/tinyme/domain/AuctionMatchingTest.java
@@ -0,0 +1,91 @@
+package ir.ramtung.tinyme.domain;
+
+import ir.ramtung.tinyme.config.MockedJMSTestConfig;
+import ir.ramtung.tinyme.domain.entity.*;
+import ir.ramtung.tinyme.domain.service.Matcher;
+import ir.ramtung.tinyme.domain.service.OrderHandler;
+import ir.ramtung.tinyme.messaging.EventPublisher;
+import ir.ramtung.tinyme.repository.BrokerRepository;
+import ir.ramtung.tinyme.repository.SecurityRepository;
+import ir.ramtung.tinyme.repository.ShareholderRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.annotation.DirtiesContext;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@Import(MockedJMSTestConfig.class)
+@DirtiesContext
+public class AuctionMatchingTest {
+
+
+    @Autowired
+    OrderHandler orderHandler;
+
+    private Security security;
+    private Broker broker;
+    private Shareholder shareholder;
+    private OrderBook orderBook;
+    private List<Order> orders;
+    private Broker broker1;
+    @Autowired
+    private Matcher matcher;
+
+    @Autowired
+    EventPublisher eventPublisher;
+
+    @Autowired
+    SecurityRepository securityRepository;
+
+    @Autowired
+    BrokerRepository brokerRepository;
+
+    @Autowired
+    ShareholderRepository shareholderRepository;
+
+    @BeforeEach
+    void setupOrderBook() {
+        securityRepository.clear();
+        brokerRepository.clear();
+        shareholderRepository.clear();
+
+        security = Security.builder().isin("ABC").build();
+        securityRepository.addSecurity(security);
+
+        broker = Broker.builder().credit(100_000_000L).brokerId(0).build();
+        broker1 = Broker.builder().credit(100_000_000L).brokerId(1).build();
+        brokerRepository.addBroker(broker1);
+
+        shareholder = Shareholder.builder().shareholderId(1).build();
+        shareholder.incPosition(security, 100_000_000_0);
+        shareholderRepository.addShareholder(shareholder);
+
+        orderBook = security.getOrderBook();
+
+    }
+
+    @Test
+    void check_correct_iop() {
+        orders = Arrays.asList(
+                new Order(6, security, Side.SELL, 200, 15800, broker1, shareholder),
+                new Order(7, security, Side.SELL, 200, 15810, broker1, shareholder),
+                new Order(6, security, Side.BUY, 200, 15900, broker, shareholder),
+                new Order(7, security, Side.BUY, 200, 15910, broker, shareholder));
+
+        orders.forEach(order -> orderBook.enqueue(order));
+
+        long IOP = orderBook.calculateIndicativeOpeningPrice(15850);
+        assertThat(IOP).isEqualTo(15850);
+        IOP = orderBook.calculateIndicativeOpeningPrice(16000);
+        assertThat(IOP).isEqualTo(15900);
+        IOP = orderBook.calculateIndicativeOpeningPrice(15000);
+        assertThat(IOP).isEqualTo(15810);
+    }
+}
